75a81db2c4f34a18de6813324925e652889e8f6b
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-09-22 10:47:45 -0300

    index.php
    Página inicial do site

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<style type="text/css">
+		
+			
+
+				#menu ul {
+				   
+				    background-color:#340000	;
+				    list-style:none;
+				}
+
+				#menu ul li { display: inline; }
+
+				#menu ul  a {
+				    padding: 3px 10px;
+				    display: inline-block;
+				    background-color:#340000;
+				    text-decoration: none;
+				    border-bottom:3px solid red;
+		         }
+
+				#menu  li a:hover {
+					color: yellow;
+				    background-color:#A6694C;
+				    font-size: 20px;
+				}
+
+				a{
+					font-size:22px;	
+					color:white;
+				}
+				p{
+					color:white;
+					font-size: 1em;
+				}
+		
+		</style>
+	</head>
+			<body>
+			<img src="sapatos.jpg">
+				
+					<nav id="menu">
+					    <ul>
+					        <li>
+					        	<details>
+
+  									<summary style="color:white; font-size: 1.5em;">Cadastro</summary>
+							  		<p><a href=""> - Produtos</a></p>
+							  		<p><a href="">- Funcionários</a></p>
+
+								</details>
+							</li>     
+				   		</ul>
+					</nav>	
+			</body>
+</html>
