5a94f0f18d7ba0bb10173d11ed21214c6dc56a48
MilenaLB <31548548+MilenaLB@users.noreply.github.com>
2017-09-29 15:25:32 -0300

    Update usuarioincorreto.php

--- usuarioincorreto.php
+++ usuarioincorreto.php
@@ -10,7 +10,7 @@
 	<div>
 	   <h1>Ops! O usuário ou a senha estão incorretos.</h1>
 	   <h1>Você é cadastrado(a)? Se não,</h1>
-	    <a class="button" href="cadastrarusuario.php">Cadastre-se!</a> <br><br>
+	    <a class="button" href="register_user.php">Cadastre-se!</a> <br><br>
 	    <a class="button" href="login.php">Voltar À Página Principal</a>
 	</div>
 </body>
